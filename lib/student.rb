cfa4cf908c84cf4e0fa060617873c0254ea0d33b
Kevin Boston <kbos22@gmail.com>
2020-06-13 16:58:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attribute|
+    @@all.each do |student|
+      attributes_hash.each do |k, v|
+        student.send [k] = v
       
       
       
